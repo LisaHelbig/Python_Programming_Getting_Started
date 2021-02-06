552bfc78a5f1261e469da44f787f321dbc6dcd9b
LisaHelbig <78630913+LisaHelbig@users.noreply.github.com>
2021-02-05 22:31:11 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,12 +14,12 @@ def welcome_assignment_answers(question):
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking'- Use MD5 hash generator and use the answer in your code":
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking'":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 2
+        answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
     else:
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question))
     debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
     print(welcome_assignment_answers(debug_question))
-    debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking'- Use MD5 hash generator and use the answer in your code"
+    debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking'"
     print(welcome_assignment_answers(debug_question))
     debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
     print(welcome_assignment_answers(debug_question))
